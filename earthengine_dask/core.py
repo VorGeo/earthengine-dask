a04866f6f52ac49332e664b45565d00515203fff
Tyler Erickson <tyler@vorgeo.com>
2024-06-20 09:04:58 -0700

    Use high volume endpoint

--- earthengine_dask/core.py
+++ earthengine_dask/core.py
@@ -4,8 +4,6 @@
 __all__ = ['InitEarthEngine', 'ClusterGEE', 'LocalClusterGEE']
 
 # %% ../00_core.ipynb 3
-import logging
-
 import coiled
 import dask.distributed
 import ee
@@ -14,25 +12,30 @@ import google.auth
 # %% ../00_core.ipynb 4
 class InitEarthEngine(dask.distributed.WorkerPlugin):
     def __init__(self, **kwargs):
-        logging.info('InitEarthEngine init')  # This appears in the notebook output where the cluster is initiated.
+        print('InitEarthEngine.init: starting')  # This appears in the notebook output where the cluster is initiated.
         self.kwargs = kwargs
+        print(f'InitEarthEngine.init: kwargs = {kwargs}')
 
     def setup(self, worker):
-        logging.info('InitEarthEngine setup')  # This appears in the dask cluster logs.
+        # Print statements output to the dask cluster logs (viewable via Coiled dashboard)
+        print('InitEarthEngine.setup: starting')
+        print('InitEarthEngine.setup: default to using the high volume endpoint')
+        self.kwargs.setdefault('opt_url', 'https://earthengine-highvolume.googleapis.com')
         import ee
+        print(f'InitEarthEngine.setup: ee.Initialize(**{self.kwargs})')
         ee.Initialize(**self.kwargs)
 
 
 class ClusterGEE(coiled.Cluster):
     def __init__(self, **kwargs):
-        logging.debug('ClusterGEE init')
+        print('ClusterGEE init')
         super().__init__(**kwargs)
         # Wait for the workers to start, then send the ADCs
         self.wait_for_workers(kwargs['n_workers'])
         coiled.credentials.google.send_application_default_credentials(self)
 
     def get_client(self):
-        logging.debug('ClusterGEE get_client')
+        print('ClusterGEE get_client')
         client = super().get_client()
         client.register_plugin(InitEarthEngine())
         return client
@@ -41,11 +44,11 @@ class ClusterGEE(coiled.Cluster):
 # For local development
 class LocalClusterGEE(dask.distributed.LocalCluster):    
     def __init__(self, **kwargs):
-        logging.debug('LocalClusterGEE init')
+        print('LocalClusterGEE init')
         super().__init__(**kwargs)
 
     def get_client(self):
-        logging.debug('LocalClusterGEE get_client')
+        print('LocalClusterGEE get_client')
         client = super().get_client()
         client.register_plugin(InitEarthEngine())
         return client
